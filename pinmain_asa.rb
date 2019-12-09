46aa9f091796b70742b20f0c42a95fe547121f5a
tatsumaru1212 <himeasa55@gamil.com>
2019-12-09 15:07:27 +0000

    asahiro

--- pinmain_asa.rb
+++ pinmain_asa.rb
@@ -7,58 +7,76 @@ require_remote 'bumper.rb'
 require_remote 'r-flipper.rb'
 require_remote 'l-flipper.rb'
 
-
-Image.register(:player, 'images/player.png') 
+Image.register(:player, 'images/ball.png') 
 Image.register(:enemy, 'images/enemy.png')
 Image.register(:flipper, 'images/flipper.png')
 
 Window.load_resources do
-  Window.width  = 600
+    
+  Window.width  = 400
   Window.height = 800
 
+  #画像関連-------------------------------------
   player_img = Image[:player]
   flipper_img = Image[:flipper]
   player_img.set_color_key([0, 0, 0])
-
   enemy_img = Image[:enemy]
   enemy_img.set_color_key([0, 0, 0])
+  #---------------------------------------------
 
-  player = Player.new(400, 50, player_img,-1, 1)
-  flipper_r = Rflipper.new(130, 500, flipper_img)
-  flipper_l = Lflipper.new(30, 500, flipper_img)
-
-  players = [player]
+  #配列---------------------------------------
+  players = []
   enemies = []
-  flippers_r = [flipper_r]
-  flippers_l = [flipper_l]
-  10.times do
-    enemies << Enemy.new(rand(600), rand(800), enemy_img)
-  end
+  flippers_r = []
+  flippers_l = []
+  #---------------------------------------------
+  
+  #実体の生成-----------------------------------
+  players << Player.new(400, 50, player_img,-1, 1)
+  flippers_r << Rflipper.new(390, 700, flipper_img)
+  flippers_l << Lflipper.new(10, 700, flipper_img)
+  enemies << Enemy.new(200, 60, enemy_img)
+  enemies << Enemy.new(100, 200, enemy_img)
+  enemies << Enemy.new(300, 200, enemy_img)
+  #---------------------------------------------
 
+
+  #ループ
   Window.loop do
-    #if Input.key_push?(K_R)
-   #     break
-    #end
-    Sprite.update(enemies)
-    Sprite.draw(enemies)
-    if Input.key_down?(K_SPACE)
-        Sprite.update(players)
+      
+    #操作----------------------------------------
+    if Input.key_down?(K_RIGHT)
         flippers_r[0].setflag(1)
-        flippers_l[0].setflag(1)
     else
         flippers_r[0].setflag(0)
+    end
+    if Input.key_down?(K_LEFT)
+        flippers_l[0].setflag(1)
+    else
         flippers_l[0].setflag(0)
     end
-    player.update
+    #---------------------------------------------
+    
+    #更新-----------------------------------------
+    Sprite.update(enemies)
+    Sprite.update(players)
     Sprite.update(flippers_r)
     Sprite.update(flippers_l)
+    #---------------------------------------------
     
+    #描画-----------------------------------------
     Sprite.draw(players)
-    
     Sprite.draw(flippers_r)
     Sprite.draw(flippers_l)
-
-    # 当たり判定
+    Sprite.draw(enemies)
+    #---------------------------------------------
+    
+    # 当たり判定----------------------------------
     Sprite.check(players, enemies)
+    Sprite.check(players, flippers_r)
+    Sprite.check(players, flippers_l)
+    #---------------------------------------------
+    
+    
   end
 end
